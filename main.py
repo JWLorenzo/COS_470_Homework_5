18147ba92d9562da7541ac3776681fa74673bd9e
JacobWLorenzo <jacob.w.lorenzo@maine.edu>
2024-12-06 01:48:07 -0500

    COS_470: It's working!
    Changes to be committed:
    modified:   main.py

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+import torch.utils.data as data_utils
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
 
 
 def load_Dataset(filename: str) -> pd.DataFrame:
@@ -32,62 +35,57 @@ def get_Trainingset(
     compareSet: pd.DataFrame,
     trainingPercent: float,
     trainingYears: list[int, int],
+    scaler: StandardScaler,
 ) -> pd.DataFrame:
     if not trainingPercent:
         trainingset = dataset[
             (dataset["YEAR"] != trainingYears[0])
             & (dataset["YEAR"] != trainingYears[1])
         ]
-        return trainingset.drop(columns=["YEAR", "TEAM"])
     else:
-        return dataset[~dataset.index.isin(compareSet.index)].drop(
-            columns=["YEAR", "TEAM"]
-        )
-        # Implement a difference based on the test set
+        trainingset = dataset[~dataset.index.isin(compareSet.index)]
+    # Normalize
+    features = trainingset.iloc[:, 1:13]
+    trainingset.iloc[:, 1:13] = scaler.transform(features)
+
+    return trainingset.drop(columns=["YEAR", "TEAM"])
 
 
 def get_Testset(
     dataset: pd.DataFrame, trainingPercent: float, trainingYears: list[int, int]
-) -> pd.DataFrame:
+) -> tuple[pd.DataFrame, StandardScaler]:
     if not trainingPercent:
         testset = dataset[
             (dataset["YEAR"] == trainingYears[0])
             | (dataset["YEAR"] == trainingYears[1])
         ]
-        return testset.drop(columns=["YEAR", "TEAM"])
     else:
-        return dataset.sample(frac=trainingPercent).drop(columns=["YEAR", "TEAM"])
+        testset = dataset.sample(frac=trainingPercent)
         # create a dataframe that is a random selection
 
+    scaler = StandardScaler()
+    features = testset.iloc[:, 1:13]
+    testset.iloc[:, 1:13] = scaler.fit_transform(features)
+    return testset.drop(columns=["YEAR", "TEAM"]), scaler
 
-def get_Headers(dataset: pd.DataFrame) -> list:
-    header = list(dataset)
-    return header
 
+def create_Datasets(dataset: pd.DataFrame, batch_size) -> data_utils.DataLoader:
+    loaded_Dataset = CustomDataset(dataset)
 
-def create_Datasets(
-    training_set: pd.DataFrame, test_set: pd.DataFrame, batch_size
-) -> list[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
-    train_Dataset = CustomDataset(training_set)
-    test_Dataset = CustomDataset(test_set)
-
-    train_Loader = torch.utils.data.DataLoader(
-        train_Dataset, batch_size=batch_size, shuffle=True
-    )
-    test_Loader = torch.utils.data.DataLoader(
-        test_Dataset, batch_size=batch_size, shuffle=False
+    dataset_Loader = data_utils.DataLoader(
+        dataset=loaded_Dataset, batch_size=batch_size, shuffle=True
     )
 
-    return train_Loader, test_Loader
+    return dataset_Loader
 
 
 # W,G,EFG,EFGD,TOR,TORD,ORB,DRB,FTR,FTRD,2P_O,2P_D,3P_O,3P_D,ADJ_T,YEAR,TEAM
 
 
-class CustomDataset(torch.utils.data.Dataset):
+class CustomDataset(data_utils.Dataset):
     def __init__(self, dataframe: pd.DataFrame):
-        self.data = torch.tensor(dataframe.iloc[:, 1:].values)
-        self.targets = torch.tensor(dataframe.iloc[:, 0].values)
+        self.data = torch.tensor(dataframe.iloc[:, 1:13].values, dtype=torch.float32)
+        self.targets = torch.tensor(dataframe.iloc[:, 0].values, dtype=torch.float32)
 
     def __len__(self):
         return len(self.data)
@@ -96,7 +94,7 @@ class CustomDataset(torch.utils.data.Dataset):
         return self.data[index], self.targets[index]
 
 
-def main() -> None:
+def parse_Arguments():
     parser = argparse.ArgumentParser(
         prog="Homework_5", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
@@ -126,20 +124,123 @@ def main() -> None:
         default=[2008, 2024],
         help="How much of the dataset split to training",
     )
-
-    args = parser.parse_args()
-    dataset = load_Dataset(args.file)
-    TRAINING_SPLIT_PERCENT = args.percent
-    TRAINING_SPLIT_YEARS = random.choices(range(args.years[0], args.years[1] + 1), k=2)
-    CSV_HEADER = get_Headers(dataset)
-    TEST_SET = get_Testset(dataset, TRAINING_SPLIT_PERCENT, TRAINING_SPLIT_YEARS)
-    TRAINING_SET = get_Trainingset(
-        dataset, TEST_SET, TRAINING_SPLIT_PERCENT, TRAINING_SPLIT_YEARS
+    parser.add_argument(
+        "-b",
+        "--batch",
+        nargs="?",
+        type=int,
+        const=64,
+        default=64,
+        help="Batch Size",
     )
 
-    print(CSV_HEADER)
-    print(TRAINING_SET.sample(10))
-    print(TEST_SET.sample(10))
+    parser.add_argument(
+        "-e",
+        "--epochs",
+        nargs="?",
+        type=int,
+        const=100,
+        default=100,
+        help="Number of epochs",
+    )
+
+    return parser.parse_args()
+
+
+def train(
+    dataloader: data_utils.DataLoader,
+    model: nn.Module,
+    loss_fn: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+) -> None:
+    # time for a training montage!
+    size = len(dataloader.dataset)
+    for batch, (x, y) in enumerate(dataloader):
+        x, y = x.to(device), y.to(device)
+
+        pred = model(x)
+        loss = loss_fn(pred, y.view(-1, 1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch % 10 == 0:
+            loss, current = loss.item(), batch * len(x)
+
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test(
+    dataloader: data_utils.DataLoader,
+    model: nn.Module,
+    loss_fn: nn.Module,
+    device: torch.device,
+) -> None:
+    num_batches = len(dataloader)
+    test_loss = 0
+
+    all_preds = []
+    all_true = []
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+
+            all_preds.append(pred.cpu().detach().numpy())
+            all_true.append(y.cpu().detach().numpy())
+
+            test_loss += loss_fn(pred, y.view(-1, 1)).item()
+
+    all_preds = np.concatenate(all_preds, axis=0)
+    all_true = np.concatenate(all_true, axis=0)
+
+    test_loss /= num_batches
+
+    r2 = r2_score(all_true, all_preds)
+
+    print(f"Test MSE: {test_loss:>8f} R²: {r2:.4f}")
+
+
+def main() -> None:
+    ARGS = parse_Arguments()
+    DATASET = load_Dataset(ARGS.file)
+    BATCH_SIZE = ARGS.batch
+    TRAINING_SPLIT_PERCENT = ARGS.percent
+    TRAINING_SPLIT_YEARS = random.choices(range(ARGS.years[0], ARGS.years[1] + 1), k=2)
+    TEST_FRAME, SCALER = get_Testset(
+        DATASET, TRAINING_SPLIT_PERCENT, TRAINING_SPLIT_YEARS
+    )
+    TRAINING_FRAME = get_Trainingset(
+        DATASET, TEST_FRAME, TRAINING_SPLIT_PERCENT, TRAINING_SPLIT_YEARS, SCALER
+    )
+    EPOCHS = ARGS.epochs
+
+    train_Loader = create_Datasets(TRAINING_FRAME, BATCH_SIZE)
+    test_Loader = create_Datasets(TEST_FRAME, BATCH_SIZE)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.manual_seed(0)
+
+    MODEL = nn.Sequential(
+        nn.Linear(12, BATCH_SIZE),
+        nn.ReLU(),
+        nn.Linear(BATCH_SIZE, BATCH_SIZE),
+        nn.ReLU(),
+        nn.Linear(BATCH_SIZE, 1),
+    ).to(device)
+
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.SGD(MODEL.parameters(), lr=0.001)
+
+    # Simulating training
+
+    for t in range(EPOCHS):
+        print(f"Epoch {t+1}\n")
+        train(train_Loader, MODEL, loss_function, optimizer, device)
+        test(test_Loader, MODEL, loss_function, device)
+    print("Finished!")
 
 
 if __name__ == "__main__":
